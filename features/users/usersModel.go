cacfa8ceef834a28d0ff4f1300d7a4d1a881641f
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-04-03 23:50:14 +0530

    chore: make mobile nullable

--- features/users/usersModel.go
+++ features/users/usersModel.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name           string `json:"name" gorm:"not null"`
-	Email          string `json:"email" gorm:"unique;not null"`
-	Mobile         string `json:"mobile" gorm:"unique"`
-	Currency       string `json:"currency" gorm:"default:inr"`
-	MonthStartDate uint8  `json:"month_start_date" gorm:"not null;default:1"`
-	WeekStartDay   string `json:"week_start_day" gorm:"not null;default:mon"`
-	ReferCode      string `json:"refer_code" gorm:"unique;not null"`
+	Name           string  `json:"name" gorm:"not null"`
+	Email          string  `json:"email" gorm:"unique;not null"`
+	Mobile         *string `json:"mobile" gorm:"unique"`
+	Currency       string  `json:"currency" gorm:"default:inr"`
+	MonthStartDate uint8   `json:"month_start_date" gorm:"not null;default:1"`
+	WeekStartDay   string  `json:"week_start_day" gorm:"not null;default:mon"`
+	ReferCode      string  `json:"refer_code" gorm:"unique;not null"`
 }
